338f3ec62164dcee873bccb5e89e2724474d0ee2
Jason-pnt <ci@pntsports.cn>
2020-08-14 17:34:40 +0800

    update

--- getIndex.py
+++ getIndex.py
@@ -26,62 +26,65 @@ chrome_options.add_argument('--ignore-certificate-errors')
 chrome_options.add_argument('--start-maximized')
 
 driver = webdriver.Chrome(chrome_options=chrome_options)
-driver.get('https://www.amazon.com/?currency=USD&language=en_US')
-time.sleep(15)
-driver.execute_script("document.body.style.zoom='0.9'")
+# driver.get('https://www.amazon.com/?currency=USD&language=en_US')
+# time.sleep(15)
+# driver.execute_script("document.body.style.zoom='0.9'")
+driver.get('https://ip138.com/')
+print(driver.find_element_by_xpath('/html/body/p[1]').text)
 
-counts = 0
-final_result = {}
-with open('test.csv','r') as f:
-    reader = csv.reader(f)
-    for row in reader:
-        if (len(row) != 0):
-            lineToStr = row[0]
-            lineToList = lineToStr.split(' ')
-            linkStr='s?k='
-            for i in range(len(lineToList)):
-                linkStr= linkStr + lineToList[i] + '+'
-                # picPath = picPath + lineToList[i] +'_'
-            linkStr= linkStr[:-1]
-            # picPath = picPath[:-1].replace('/','-')
-            driver.maximize_window()
-            counts = 1 + counts
-            flags = False
-            keyword = linkStr.replace('+', ' ')[4:]
-            print(str(counts) + ' for  keyword: ' + keyword)
-            if (keyword not in final_result):
-                final_result[keyword] = [0, 0, 0]
-            for i in range(1,2):
-                # print('page :' + str(i))
-                if (final_result[keyword][1] != 0 and final_result[keyword][2] != 0):
-                    break
-                driver.get('https://www.amazon.com/' + linkStr + '&page=' + str(i)+ '&language=en_US')            
-                if flags == False:
-                    count = driver.find_element_by_xpath('//*[@id="search"]/span/div/span/h1/div/div[1]/div/div/span[1]').text
-                    count = count.split(' ')[-3].replace(',','')
-                    if final_result[keyword][0] == 0:
-                        final_result[keyword][0] = count
-                    flags = True
-                soup = BeautifulSoup(driver.page_source, "html.parser")
-                for asin in soup.find_all(href=re.compile("READY-PARD-")):
-                    links = str(asin.get('href'))
-                    if (links.startswith('/gp')):
-                        # adver 
-                        adver = links.split('sr_1_')[1].split('_')[0]
-                        if final_result[keyword][1] == 0:
-                            final_result[keyword][1] = adver
-                    else:
-                        nature = links.split('sr_1_')[1].split('?')[0]
-                        # nature
-                        if final_result[keyword][2] == 0:
-                            final_result[keyword][2] = nature
-driver.quit()
-print("\n\n")
-for k,v in final_result.items():
-    print(k,v)
+# counts = 0
+# final_result = {}
+# with open('test.csv','r') as f:
+#     reader = csv.reader(f)
+#     for row in reader:
+#         if (len(row) != 0):
+#             lineToStr = row[0]
+#             lineToList = lineToStr.split(' ')
+#             linkStr='s?k='
+#             for i in range(len(lineToList)):
+#                 linkStr= linkStr + lineToList[i] + '+'
+#                 # picPath = picPath + lineToList[i] +'_'
+#             linkStr= linkStr[:-1]
+#             # picPath = picPath[:-1].replace('/','-')
+#             driver.maximize_window()
+#             counts = 1 + counts
+#             flags = False
+#             keyword = linkStr.replace('+', ' ')[4:]
+#             print(str(counts) + ' for  keyword: ' + keyword)
+#             if (keyword not in final_result):
+#                 final_result[keyword] = [0, 0, 0]
+#             for i in range(1,2):
+#                 # print('page :' + str(i))
+#                 if (final_result[keyword][1] != 0 and final_result[keyword][2] != 0):
+#                     break
+#                 driver.get('https://www.amazon.com/' + linkStr + '&page=' + str(i)+ '&language=en_US')            
+#                 if flags == False:
+#                     count = driver.find_element_by_xpath('//*[@id="search"]/span/div/span/h1/div/div[1]/div/div/span[1]').text
+#                     count = count.split(' ')[-3].replace(',','')
+#                     if final_result[keyword][0] == 0:
+#                         final_result[keyword][0] = count
+#                     flags = True
+#                 soup = BeautifulSoup(driver.page_source, "html.parser")
+#                 for asin in soup.find_all(href=re.compile("READY-PARD-")):
+#                     links = str(asin.get('href'))
+#                     if (links.startswith('/gp')):
+#                         # adver 
+#                         adver = links.split('sr_1_')[1].split('_')[0]
+#                         if final_result[keyword][1] == 0:
+#                             final_result[keyword][1] = adver
+#                     else:
+#                         nature = links.split('sr_1_')[1].split('?')[0]
+#                         # nature
+#                         if final_result[keyword][2] == 0:
+#                             final_result[keyword][2] = nature
+# driver.quit()
+# print("\n\n")
+# for k,v in final_result.items():
+#     print(k,v)
 
-pf = pd.DataFrame(final_result)
-pf = pd.DataFrame(pf.values.T, index= pf.columns, columns=pf.index)
-file_path = pd.ExcelWriter('asin-top.xlsx')
-pf.to_excel(file_path,encoding='utf-8',index=True)
-file_path.save()
+# pf = pd.DataFrame(final_result)
+# pf = pd.DataFrame(pf.values.T, index= pf.columns, columns=pf.index)
+# file_path = pd.ExcelWriter('asin-top.xlsx')
+# pf.to_excel(file_path,encoding='utf-8',index=True)
+# file_path.save()
+
